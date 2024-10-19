f5a7d6f31c65cd65c45f92cc0ae7fba6686524ad
iamethanglenny <ethanglenny@gmail.com>
2024-10-19 12:56:46 +0200

    FEAT: Add all functions to allow for all buttons to work correctly and for calculation to handle a single operator calculation. NEXT: Readjust code to allow for multiple operator calculations to occur.

--- script.js
+++ script.js
@@ -1,51 +1,76 @@
-let firstNum = '';
+let currentValue = '';
 let operator = '';
-let secondNum = '';
-
+let storedValue = '';
+let isNewValue = false;
 
 // Basic functions to be called later in the application
-function sum(firstNum, secondNum) {
-    return (firstNum + secondNum);
+function sum(a, b) {
+    return (a + b);
 }
 
-function subtract(firstNum, secondNum) {
-    return (firstNum - secondNum);
+function subtract(a, b) {
+    return (a - b);
 }
 
-function divide(firstNum, secondNum) {
-    if (secondNum === 0) {
+function divide(a, b) {
+    if (b === 0) {
         return 'lmao';
     } else {
-        return (firstNum / secondNum)};
+        return (a / b)};
 }
 
 function multiply(firstNum, secondNum) {
     return (firstNum * secondNum);
 }
 
-function clear() {
-    let firstNum = '';
-    let operator = '';
-    let secondNum = '';
+let displayContent = document.getElementById("display");
+let displayValue = '';
+
+function addToDisplay(value) {
+    if (displayValue.length <= 8) {
+        displayValue += value;
+        displayContent.innerText = displayValue;
+    }
+}
+
+function clearDisplay() {
+    displayValue = '';
+    firstNum = '';
+    operator = '';
+    secondNum = '';
+    isSecondNum = false;
+    displayContent.innerText = '0';
 }
 
 function decimal() {
-
+    if (!displayValue.includes('.')) {
+        if (displayValue === '0') {
+            displayValue === '0.';
+        } else {
+            displayValue += '.';
+        }
+        displayContent.innerText = displayValue;
+    }
 }
 
 function percentage() {
-
+    if (displayValue === '') return;
+    const numberValue = parseFloat(displayValue);
+    displayValue = (numberValue / 100).toFixed(9).toString();
+    displayContent.innerText = displayValue;
 }
 
 function positiveNegative() {
-
-}
-
-function display() {
-
+    if (displayValue === '') return;
+    const numberValue = parseFloat(displayValue);
+    displayValue = (numberValue * -1).toString();
+    displayContent.innerText = displayValue;
 }
 
 function operate(firstNum, operator, secondNum) {
+    firstNum = parseFloat(firstNum);
+    secondNum = parseFloat(secondNum);
+
     if (operator === '+') {
         return sum(firstNum, secondNum);
     } else if (operator === '-') {
@@ -55,8 +80,24 @@ function operate(firstNum, operator, secondNum) {
     } else if (operator === '*') {
         return multiply(firstNum, secondNum);
     } else {
-        return 'lmao'
+        return 'error';
     }
 }
 
-console.log(operate(5, '', 3))
+function selectOperator(op) {
+    if (!isSecondNum) {
+        firstNum = displayValue;
+        operator = op;
+        displayValue = '';
+        isSecondNum = true;
+    }
+}
+
+function calculate() {
+    if (isSecondNum && displayValue !== '') {
+        secondNum = displayValue;
+        const result = operate(firstNum, operator, secondNum);
+        displayContent.innerText = result.toString();
+        isSecondNum = false;
+    }
+}
