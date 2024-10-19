87af796d416826138a92f5a003ce0f7bf91f83b7
iamethanglenny <ethanglenny@gmail.com>
2024-10-19 07:48:13 +0200

    FEAT: Add basic functinos to perform standard calculation operations

---
+++ script.js
@@ -0,0 +1,26 @@
+let firstNum = '';
+let operator = '';
+let secondNum = '';
+
+
+// Basic functions to be called later in the application
+function sum(firstNum, secondNum) {
+    return (firstNum + secondNum);
+}
+console.log(sum(3, 5))
+
+function subtract(firstNum, secondNum) {
+    return (firstNum - secondNum);
+}
+console.log(subtract(3, 5))
+
+function divide(firstNum, secondNum) {
+    return (firstNum / secondNum);
+}
+console.log(divide(3, 5))
+
+function multiply(firstNum, secondNum) {
+    return (firstNum * secondNum);
+}
+console.log(multiply(3, 5))
+
